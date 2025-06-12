17af95f5e16631f0c5f90e4fc5a1a82358356988
sanhue903 <sesanhueza2019@udec.cl>
2025-06-12 17:02:10 -0400

    Delete comments

--- gensim/gensim.cpp
+++ gensim/gensim.cpp
@@ -32,19 +32,19 @@ namespace gensim {
             if (line.empty()) continue;
 
             if (line[0] == '>') {
-                // Si venimos leyendo una secuencia, la guardamos
+
                 if (!current_sequence.str().empty()) {
                     sequences.push_back(current_sequence.str());
                     current_sequence.str("");
                     current_sequence.clear();
                 }
-                continue; // Ignora línea del encabezado
+                continue; 
             } else {
                 current_sequence << line;
             }
         }
 
-        // Guarda la última secuencia si el archivo no termina con '>'
+        
         if (!current_sequence.str().empty()) {
             sequences.push_back(current_sequence.str());
         }

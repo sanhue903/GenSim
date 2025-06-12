d0105725259599234098c9798df60406d9756a6b
sanhue903 <sesanhueza2019@udec.cl>
2025-06-12 15:42:06 -0400

    tests fasta files

--- tests/test_base.cpp
+++ tests/test_base.cpp
@@ -1,7 +1,18 @@
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-#include "gensim/gensim.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <gensim.hpp>
 
-TEST_CASE("base test", "[foo]") {
-    REQUIRE(gensim::foo(42) == True); // Assuming foo returns the input value for testing purposes
+
+TEST_CASE("read_fasta reads multiple sequences", "[fasta]") {
+    // Crear archivo FASTA de prueba
+    std::string test_file = "tests/test_sample.fasta";
+    std::ofstream out(test_file);
+    out << ">seq1\nACTGACTG\n>seq2\nCGTACGTA\n";
+    out.close();
+
+    auto sequences = gensim::read_fasta(test_file);
+
+    REQUIRE(sequences.size() == 2);
+    REQUIRE(sequences[0] == "ACTGACTG");
+    REQUIRE(sequences[1] == "CGTACGTA");
 }
